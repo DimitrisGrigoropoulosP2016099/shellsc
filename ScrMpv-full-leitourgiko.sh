e3c1e97f1d18f8a89a5dc494b34ec01c1b66549b
DimitrisGrigoropoulos <79205642+DimitrisGrigoropoulosP2016099@users.noreply.github.com>
2022-01-26 19:37:01 +0200

    Add files via upload

---
+++ ScrMpv-full-leitourgiko.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#P2016099
+
+FirstOpt="video noVideo exit"
+SecondOpt="record No"
+
+select option in $FirstOpt; do
+    if [ "$option" == "video" ]; then
+        echo -n "search: "
+        read -r search
+        id="$(youtube-dl ytsearch:"$search" --get-id)"
+        link="https://www.youtube.com/watch?v=${id}"
+        select opt in $SecondOpt; do
+            if [ "$opt" == "record" ]; then
+                echo -n "Name file: "
+                read -r name
+                echo "Now playing "$(youtube-dl "$link" --get-title)""
+                mpv --cache=yes --stream-record="$name".mp4 --ytdl-format=bestvideo[height "$link" <=?1080]+bestaudio/best
+                mpv $name.mp4 -o $name.mp3 
+                rm $name.mp4
+                break
+            elif [ "$opt" == "No" ]; then
+                echo " Now playing "$(youtube-dl "$link" --get-title)""
+                mpv --cache=yes --ytdl-format=bestvideo[height "$link" <=?1080]+bestaudio/best
+                break
+            fi
+        done
+    elif [ "$option" == "noVideo" ]; then
+        echo -n "search: "
+        read -r search
+        id="$(youtube-dl ytsearch:"$search" --get-id)"
+        link="https://www.youtube.com/watch?v=${id}"
+        select opt in $SecondOpt; do
+            if [ "$opt" == "record" ]; then
+                echo -n "Name file: "
+                read -r name
+                echo "Now playing "$(youtube-dl "$link" --get-title)""
+                mpv --cache=yes --video=no --stream-record="$name".mp4 --ytdl-format=bestaudio/best "$link"
+                mpv $name.mp4 -o $name.mp3 --no-video
+                rm $name.mp4
+                break
+            elif [ "$opt" == "No" ]; then
+                echo "Now playing "$(youtube-dl "$link" --get-title)""
+                mpv --cache=yes --video=no --ytdl-format=bestaudio/best "$link"
+                break
+            fi
+        done
+    elif [ "$option" == "exit" ]; then
+        break
+    fi
+done
